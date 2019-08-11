131c901adc5297bca8900ff0df1ec5f4446e5f8c
SpicyIslandFruits <spicyisland1998@gmail.com>
2019-08-11 19:44:46 +0000

    finished django tutorial

---
+++ snippets/serializers.py
@@ -0,0 +1,24 @@
+"""
+serializers provides a mechanism for “translating” Django models into other formats such as json or sql data
+"""
+from rest_framework import serializers
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
